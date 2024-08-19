e924abde56d35ac06af12206d5ce512d95d11a17
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2024-08-20 00:51:18 +0300

    feat: handle routing of `Login`, `Register` components
    handle routing of `Login`, and `Register` components, and add
    Authentication context

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import MainLayout from './pages/MainLayout/MainLayout';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import NotFound from './pages/NotFound/NotFound';
+import AuthContextProvider from './context/Auth/Auth';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '',
       element: <MainLayout />,
-      children: [],
+      children: [
+        { path: 'login', element: <Login /> },
+        { path: 'register', element: <Register /> },
+        { path: '*', element: <NotFound /> },
+      ],
     },
   ]);
-  return <RouterProvider router={router} />;
+  return (
+    <AuthContextProvider>
+      <RouterProvider router={router} />
+    </AuthContextProvider>
+  );
 }
 
 export default App;
