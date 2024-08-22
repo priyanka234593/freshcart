55b113719a187d07353c6f7df1b7f7bca309179b
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2024-08-22 06:13:02 +0300

    feat: add logout btn logic and add new home btn

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,11 @@ import { useContext } from 'react';
 import { authContext } from '../../context/Auth/Auth';
 
 export default function Navbar() {
-  const { userToken } = useContext(authContext);
+  const { userToken, setUserToken } = useContext(authContext);
+  function logout() {
+    setUserToken(null);
+    localStorage.removeItem('authToken');
+  }
   return (
     <>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -15,6 +19,18 @@ export default function Navbar() {
           >
             <img src={logo} className="h-8" alt="Site Logo" />
           </a>
+          {userToken && (
+            <ul className='font-medium flex flex-col mr-auto ml-4 p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-3 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"'>
+              <li>
+                <Link
+                  to=""
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  <i className="fa-solid fa-home" /> Home
+                </Link>
+              </li>
+            </ul>
+          )}
           <button
             data-collapse-toggle="navbar-default"
             type="button"
@@ -41,14 +57,6 @@ export default function Navbar() {
           </button>
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-3 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              {/* <li>
-                <Link
-                  href="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                  aria-current="page">
-                  <i className="fa-brands fa-instagram"></i>
-                </Link>
-              </li> */}
               <li>
                 <Link
                   href="#"
@@ -99,10 +107,10 @@ export default function Navbar() {
               </li>
               {userToken ? (
                 <>
-                  {/* To be handled later */}
                   <li>
                     <Link
                       to="login"
+                      onClick={logout}
                       className="block py-2 px-3 text-gray-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     >
                       Logout
