cf7840b923757972fc048d118b6b6f569d4752f0
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2024-08-22 06:04:16 +0300

    feat: initialize userToken with localStorage value
    Initializes the `userToken` state in with the value retrieved from
    `localStorage` if it exists to ensure authentication
    even after page reload.

--- src/context/Auth/Auth.jsx
+++ src/context/Auth/Auth.jsx
@@ -1,9 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from 'react';
 
 export const authContext = createContext(null);
 
 export default function AuthContextProvider(props) {
   const [userToken, setUserToken] = useState(null);
+
+  useEffect(() => {
+    if (localStorage.getItem('authToken')) {
+      setUserToken(localStorage.getItem('authToken'));
+    }
+  }, []);
+
   return (
     <authContext.Provider value={{ userToken, setUserToken }}>
       {props.children}
