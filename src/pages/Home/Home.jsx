f11a3a60080918c4215e7c24057d9a3a5cbde685
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2024-08-24 19:57:25 +0300

    feat: add `CategorySider` component into `Home` page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import CategorySlider from '../../components/CategorySlider/CategorySlider';
 import Products from '../../components/Products/Products';
 
 export default function Home() {
   return (
     <>
+      <CategorySlider />
       <Products />
     </>
   );
