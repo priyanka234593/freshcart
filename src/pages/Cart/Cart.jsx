72386fa42de679f19a8dc0ba4518ac6cdd78b36c
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2024-09-03 03:43:47 +0300

    feat: add empty card and loading indicators

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { cartContext } from '../../context/Cart/Cart';
 import { Link } from 'react-router-dom';
+import Spinner from '../../components/Spinner/Spinner';
 
 export default function Cart() {
   const { getProducts, deleteProduct, updateProductQuantity } =
@@ -51,102 +52,124 @@ export default function Cart() {
             </tr>
           </thead>
           <tbody>
-            {data?.products?.map((product) => (
-              <tr
-                key={product._id}
-                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-              >
-                <td className="p-4">
-                  <img
-                    src={product.product.imageCover}
-                    className="w-16 md:w-32 max-w-full max-h-full rounded-lg"
-                    alt="Apple Watch"
-                  />
-                </td>
-                <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                  {product.product.title}
-                </td>
-                <td className="px-6 py-4">
-                  <div className="flex items-center">
-                    <button
-                      onClick={() => {
-                        handleUpdateProductQuantity(
-                          product.product._id,
-                          product.count - 1
-                        );
-                      }}
-                      className="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                      type="button"
-                    >
-                      <span className="sr-only">Quantity button</span>
-                      <svg
-                        className="w-3 h-3"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 18 2"
-                      >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M1 1h16"
-                        />
-                      </svg>
-                    </button>
-                    <div>
-                      <input
-                        type="number"
-                        id="first_product"
-                        disabled
-                        className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder={product.count}
-                        required
+            {data ? (
+              data.products.length === 0 ? (
+                <tr>
+                  <td
+                    colSpan={5}
+                    className="text-center text-3xl h-20 font-bold"
+                  >
+                    <i className="fas fa-box-open me-3"></i>
+                    Wow, such empty!
+                  </td>
+                </tr>
+              ) : (
+                data.products.map((product) => (
+                  <tr
+                    key={product._id}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                  >
+                    <td className="p-4">
+                      <img
+                        src={product.product.imageCover}
+                        className="w-16 md:w-32 max-w-full max-h-full rounded-lg"
+                        alt="Apple Watch"
                       />
-                    </div>
-                    <button
-                      onClick={() => {
-                        handleUpdateProductQuantity(
-                          product.product._id,
-                          product.count + 1
-                        );
-                      }}
-                      className="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                      type="button"
-                    >
-                      <span className="sr-only">Quantity button</span>
-                      <svg
-                        className="w-3 h-3"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 18 18"
+                    </td>
+                    <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                      {product.product.title}
+                    </td>
+                    <td className="px-6 py-4">
+                      <div className="flex items-center">
+                        <button
+                          onClick={() => {
+                            handleUpdateProductQuantity(
+                              product.product._id,
+                              product.count - 1
+                            );
+                          }}
+                          className="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                          type="button"
+                        >
+                          <span className="sr-only">Quantity button</span>
+                          <svg
+                            className="w-3 h-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 2"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M1 1h16"
+                            />
+                          </svg>
+                        </button>
+                        <div>
+                          <input
+                            type="number"
+                            id="first_product"
+                            disabled
+                            className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder={product.count}
+                            required
+                          />
+                        </div>
+                        <button
+                          onClick={() => {
+                            handleUpdateProductQuantity(
+                              product.product._id,
+                              product.count + 1
+                            );
+                          }}
+                          className="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                          type="button"
+                        >
+                          <span className="sr-only">Quantity button</span>
+                          <svg
+                            className="w-3 h-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 18"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M9 1v16M1 9h16"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                      ${product.price * product.count}
+                    </td>
+                    <td className="px-6 py-4">
+                      <button
+                        onClick={() => handleDeleteProduct(product.product._id)}
+                        className="font-medium text-red-600 dark:text-red-500 hover:underline"
                       >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M9 1v16M1 9h16"
-                        />
-                      </svg>
-                    </button>
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              )
+            ) : (
+              <tr>
+                <td colSpan="5" className="py-4">
+                  <div>
+                    <Spinner />
                   </div>
                 </td>
-                <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                  ${product.price * product.count}
-                </td>
-                <td className="px-6 py-4">
-                  <button
-                    onClick={() => handleDeleteProduct(product.product._id)}
-                    className="font-medium text-red-600 dark:text-red-500 hover:underline"
-                  >
-                    Remove
-                  </button>
-                </td>
               </tr>
-            ))}
+            )}
           </tbody>
         </table>
       </div>
