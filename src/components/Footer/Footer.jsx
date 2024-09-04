1e65760ed4aaec4894ad9b1596d7b4ce8e7c983d
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2024-09-04 19:16:51 +0300

    feat: add site footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,155 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/freshcart-logo.svg';
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="bg-white border border-t-1 mt-6 dark:bg-gray-900">
+        <div className=" p-6 mx-auto">
+          <div className="lg:flex">
+            <div className="w-full -mx-6 lg:w-2/5">
+              <div className="px-6">
+                <a href="#">
+                  <img className="w-auto h-7" src={logo} alt />
+                </a>
+                <p className="max-w-sm mt-2 text-gray-500 dark:text-gray-400">
+                  Discover More, Spend Less – Shop the Best at Your Fingertips!
+                </p>
+              </div>
+            </div>
+            <div className="mt-6 lg:mt-0 lg:flex-1">
+              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+                <div>
+                  <h3 className="text-gray-700 uppercase dark:text-white">
+                    Other Sites
+                  </h3>
+                  <a
+                    href="https://mohamedemary.github.io/route-frontend/assignments/10-daniels/index.html"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
+                  >
+                    Daniels
+                  </a>
+                  <a
+                    href="https://mohamedemary.github.io/route-frontend/assignments/07-devFolio/index.html"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
+                  >
+                    Devfolio
+                  </a>
+                  <a
+                    href="https://game-over-ivory.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
+                  >
+                    Game Over
+                  </a>
+                  <a
+                    href="https://todo-app-wine-tau.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
+                  >
+                    Todo App
+                  </a>
+                </div>
+                <div>
+                  <h3 className="text-gray-700 uppercase dark:text-white">
+                    Jump to
+                  </h3>
+                  <Link
+                    to="/"
+                    className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
+                  >
+                    <i className="fa-fw  fas fa-home"></i> Home
+                  </Link>
+                  <Link
+                    to="/wishlist"
+                    className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
+                  >
+                    <i className="fa-fw  fas fa-heart"></i> Wishlist
+                  </Link>
+                  <Link
+                    to="/cart"
+                    className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
+                  >
+                    <i className="fa-fw  fas fa-shopping-cart"></i> Cart
+                  </Link>
+                </div>
+                <div>
+                  <h3 className="text-gray-700 uppercase dark:text-white">
+                    Contact
+                  </h3>
+                  <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">
+                    <a href="mailto:mohamed.ahmed.emary@gmail.com">
+                      mohamed.ahmed.emary@gmail.com
+                    </a>
+                  </span>
+                  <span className="block space-x-2 mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">
+                    <a
+                      href="https://linkedin.com/in/mohamedemary"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i className="fa-lg fa-fw  fab fa-linkedin"></i>
+                    </a>
+                    <a
+                      href="https://github.com/mohamedemary"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i className="fa-lg fa-fw  fab fa-github"></i>
+                    </a>
+                    <a
+                      href="mailto:mohamed.ahmed.emary@gmail.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i className="fa-lg fa-fw  far fa-envelope"></i>
+                    </a>
+                    <a
+                      href="https://leetcode.com/Spark71"
+                      className="inline"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <svg
+                        className="inline text-sm"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="14"
+                        height="14"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M13.483 0a1.37 1.37 0 0 0-.961.438L7.116 6.226l-3.854 4.126a5.3 5.3 0 0 0-1.209 2.104a5 5 0 0 0-.125.513a5.5 5.5 0 0 0 .062 2.362a6 6 0 0 0 .349 1.017a5.9 5.9 0 0 0 1.271 1.818l4.277 4.193l.039.038c2.248 2.165 5.852 2.133 8.063-.074l2.396-2.392c.54-.54.54-1.414.003-1.955a1.38 1.38 0 0 0-1.951-.003l-2.396 2.392a3.02 3.02 0 0 1-4.205.038l-.02-.019l-4.276-4.193c-.652-.64-.972-1.469-.948-2.263a2.7 2.7 0 0 1 .066-.523a2.55 2.55 0 0 1 .619-1.164L9.13 8.114c1.058-1.134 3.204-1.27 4.43-.278l3.501 2.831c.593.48 1.461.387 1.94-.207a1.384 1.384 0 0 0-.207-1.943l-3.5-2.831c-.8-.647-1.766-1.045-2.774-1.202l2.015-2.158A1.384 1.384 0 0 0 13.483 0m-2.866 12.815a1.38 1.38 0 0 0-1.38 1.382a1.38 1.38 0 0 0 1.38 1.382H20.79a1.38 1.38 0 0 0 1.38-1.382a1.38 1.38 0 0 0-1.38-1.382z"
+                        />
+                      </svg>
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr className="h-px my-4 bg-gray-200 border-none dark:bg-gray-700" />
+          <div>
+            <p className="text-center text-gray-500 dark:text-gray-400">
+              <div>
+                "It does not matter how slowly you go as long as you do not
+                stop." - <span className="italic text-md">Confucius</span>
+              </div>
+            </p>
+          </div>
+          <div>
+            <p className="text-center italic py-3 text-gray-500 dark:text-gray-400">
+              <div>Made with love and passion by Mohamed Emary</div>
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
--- src/pages/MainLayout/MainLayout.jsx
+++ src/pages/MainLayout/MainLayout.jsx
@@ -5,17 +5,20 @@ import { Offline } from 'react-detect-offline';
 
 export default function MainLayout() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Offline>
         <p className="w-fit font-bold bg-red-500 rounded-lg text-white text-center p-2 fixed bottom-5 right-5 z-10">
           You are offline
         </p>
       </Offline>
       <Navbar />
-      <main className="mt-[87px]">
-        <Outlet />
+      <main className="flex-grow flex items-center mt-[87px]">
+        <div className="w-full">
+          <Outlet />
+        </div>
       </main>
       <Footer />
-    </>
+    </div>
+
   );
 }
